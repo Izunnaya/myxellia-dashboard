3e47c7b41f42becce9c517171c583fe3cbb5c53a
Izunnaya <ugorjimicheal.um@gmail.com>
2025-08-14 19:14:55 +0100

    App structure setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import Listings from "./component/Listings";
+import Navbar from "./component/navbar/navbar";
+import WelcomeBoard from "./component/welcomeBoard/WelcomeBoard";
+
 const App = () => {
-  return <div className="text-dar">App</div>;
+  return (
+    <div className="text-dark">
+      <Navbar />
+      <WelcomeBoard />
+      <Listings />
+    </div>
+  );
 };
 
 export default App;
