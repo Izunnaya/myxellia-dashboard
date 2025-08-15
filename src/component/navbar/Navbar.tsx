5d66557cff8bf4285389188126304616e0c31b6e
Izunnaya <ugorjimicheal.um@gmail.com>
2025-08-15 17:33:26 +0100

    Top bar with hover effect done

--- src/component/navbar/Navbar.tsx
+++ src/component/navbar/Navbar.tsx
@@ -1,9 +1,31 @@
 const Navbar = () => {
+  const hoverStyle =
+    "absolute text-xs bg-[#191919] p-3 mt-3  left-1/2 -translate-x-1/2 scale-0 rounded-lg transition-all group-hover:scale-100";
   return (
     <div className="w-full">
-      <div className="bg-[#191919] py-6 px-20 flex justify-between text-white items-center">
+      <div className="bg-[#191919] py-5 px-28 flex justify-between text-white items-center">
         <img src="/src/assets/logo.svg" width={153} height={100} />
-        <div>Menu icons</div>
+        <ul className="flex gap-4 items-center">
+          <li>
+            <img src="/src/assets/Notification 1.svg" alt="Notification-icon" />
+          </li>
+          <li className="relative group cursor-pointer">
+            <img src="/src/assets/Budgeting.svg" alt="Budget-icon" />
+            <span className={hoverStyle}>Budgeting</span>
+          </li>
+          <li className="relative group cursor-pointer">
+            <img src="/src/assets/Calendar.svg" alt="calendar-icon" />
+            <span className={hoverStyle}>Calender</span>
+          </li>
+          <li>
+            {" "}
+            <img src="/src/assets/message-notif.svg" alt="Message-icon" />
+          </li>
+          <li>
+            {" "}
+            <img src="/src/assets/Profile.svg" alt="profile-icon" />
+          </li>
+        </ul>
       </div>
       <div>Bottom Nav</div>
     </div>
