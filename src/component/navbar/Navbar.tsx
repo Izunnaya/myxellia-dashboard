b670729b468f1fb5f59248382220db5eb13ba9ba
Izunnaya <ugorjimicheal.um@gmail.com>
2025-08-15 16:57:04 +0100

    Navbar assets added

--- src/component/navbar/Navbar.tsx
+++ src/component/navbar/Navbar.tsx
@@ -1,12 +1,12 @@
 const Navbar = () => {
   return (
-    <>
-      <div>
-        <img></img>
+    <div className="w-full">
+      <div className="bg-[#191919] py-6 px-20 flex justify-between text-white items-center">
+        <img src="/src/assets/logo.svg" width={153} height={100} />
         <div>Menu icons</div>
       </div>
       <div>Bottom Nav</div>
-    </>
+    </div>
   );
 };
 
